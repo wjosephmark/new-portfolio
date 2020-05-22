47d55fe8cf43ea07cd3db5657ac1e07679f659ec
wjosephmark <wjosephmark@gmail.com>
2020-05-22 11:51:17 -0600

    fixed flip card issues

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -3,14 +3,30 @@ import React, { useState, useEffect } from 'react'
 export default function Projects() {
     return(
         <div className='app'>
-            <div className="flip-card">
-                <div className="flip-card-inner">
-                    <div className="flip-card-front">
-                        <h1>Devcamp Fries</h1>
-                        <img className="card-img" src="https://i.postimg.cc/cCVkgcY9/fries.png" />
+            <div className="header-wrapper">
+                <h1>Projects</h1>
+            </div>
+            <div className="flip-card-wrapper">                
+                <div class="flip-card">
+                    <div class="flip-card-inner">
+                        <div class="flip-card-front">
+                            <h1>Devcamp Fries</h1>
+                            <img className="card-img" src="https://i.postimg.cc/cCVkgcY9/fries.png" />
+                        </div>
+                        <div className="flip-card-back">
+                            <a className="link" target="_blank" href="https://jm-fries.herokuapp.com/">Visit Devcamp Fries</a>
+                        </div>
                     </div>
-                    <div className="flip-card-back">
-                        <img src="./style/pictures/fries.png" />
+                </div>
+                <div class="flip-card">
+                    <div class="flip-card-inner">
+                        <div class="flip-card-front">
+                            <h1>Devcamp Fries</h1>
+                            <img className="card-img" src="https://i.postimg.cc/cCVkgcY9/fries.png" />
+                        </div>
+                        <div className="flip-card-back">
+                            <a className="link" target="_blank" href="https://jm-fries.herokuapp.com/">Visit Devcamp Fries</a>
+                        </div>
                     </div>
                 </div>
             </div>
