2e902a8da948e5fe1c0bebf1b380daea49e291af
wjosephmark <wjosephmark@gmail.com>
2020-05-27 17:12:18 -0600

    first heroku push

--- src/pages/about-me.js
+++ src/pages/about-me.js
@@ -7,13 +7,16 @@ export default function AboutMe(){
                 <div className="heading-wrapper">
                     <h1>About Me</h1>
                 </div>
+                <div className="spacer" />
                 <div className="photo-wrapper">
                     <img src="https://avatars2.githubusercontent.com/u/61288503?s=460&u=ab9865db546865c0bbbb84e13d11c108ae385a5f&v=4" />
                 </div>
                 <div className="spacer" />
                 <div className="text-wrapper">
                     <p>Hello, <br /> I am William Mark but I go by Joseph.  I recently completed a coding bootcamp at Bottega and I am currently
-                    looking to start my career as a programmer.  I have been coding for almost a year and I fall more in love with it everyday.</p>
+                    looking to start my career as a programmer.  I have been coding for almost a year and I fall more in love with it everyday.
+                    I love hiking, mountain biking, skiing, rock climbing, and anything else outdoors.  I spend a lot of time with my friends and
+                    family too.  I pride myself in being a happy and positive person and creating a fun and comfortable environment wherever I am.</p>
                 </div>
             </div>
         </div>
